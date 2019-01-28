0827825b122d3c77fb283472f27268b915e76b5d
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-28 14:35:28 -0500

    Update data_preprosessing.py

--- data_preprosessing.py
+++ data_preprosessing.py
@@ -15,3 +15,10 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
 
+#taking care of missed data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN' , strategy ='mean', axis =0)
+### calculates the mean
+imputer.fit(X[:, 1:3])
+### put it in the matrics
+X[:, 1:3] = imputer.transform(X[:, 1:3])
