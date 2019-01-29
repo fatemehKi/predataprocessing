3a84f15f0e42d2f4a9b205ea04e8c9efddd9f14f
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-29 09:09:08 -0500

    Update data_preprosessing.py

--- data_preprosessing.py
+++ data_preprosessing.py
@@ -29,5 +29,6 @@ labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features =[0])
 X = onehotencoder.fit_transform(X).toarray()
-
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)
 
