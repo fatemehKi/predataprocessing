0f9061929d0fc9ee8d4c5a8cadc5fa028ee0bc7a
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-29 09:49:29 -0500

    Update data_preprocessing.R

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -12,6 +12,10 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Salary)
                         
-ataset$Country = factor(dataset$Country,
+dataset$Country = factor(dataset$Country,
                          levels = c('France', 'Spain', 'Germany'),
                          labels = c(1, 2, 3))
+                        
+dataset$Purchased = factor(dataset$Purchased,
+                         levels = c('No', 'Yes'),
+                         labels = c(0, 1))
