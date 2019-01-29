fcdb0a7e1152fa45cc8c26fe90b6a0b560f1ed23
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-29 00:51:37 -0500

    Update data_preprocessing.R

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -2,8 +2,12 @@
 
 dataset = read.csv('Data.csv')
 
-#taking care of missing data
+#taking care of missing data in Age
 dataset$Age = ifelse(is.na(dataset$Age),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Age)
 
+#taking care of missing data in Salary
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Salary)
