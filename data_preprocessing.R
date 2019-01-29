3dcd209249d8cabda1f7bd295ee20deb6bd6249d
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-29 11:18:50 -0500

    Update data_preprocessing.R

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -19,8 +19,7 @@ dataset$Country = factor(dataset$Country,
 dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No', 'Yes'),
                          labels = c(0, 1))
-
-                        
+                       
 #Splitting the edataset
 #install.packages('caTools')
 library(caTools)
