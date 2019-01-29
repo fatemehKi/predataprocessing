215ade68ba6375e1ac3779025ea2b41cbecf6c26
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-29 11:20:47 -0500

    Update data_preprocessing.R

--- data_preprocessing.R
+++ data_preprocessing.R
@@ -25,3 +25,5 @@ dataset$Purchased = factor(dataset$Purchased,
 library(caTools)
 set.seed(123)
 split= sample.split(dataset$Purchased, SplitRatio = 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
